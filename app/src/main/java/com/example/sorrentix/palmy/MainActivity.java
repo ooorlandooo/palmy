a92addf57bab0d9ecb1a5ce847af392952a3d45b
Orlando Aprea <o.aprea@gmail.com>
2017-06-27 17:35:33 +0200

    MainActivity: correct syntax error

--- app/src/main/java/com/example/sorrentix/palmy/MainActivity.java
+++ app/src/main/java/com/example/sorrentix/palmy/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("ciao from branch“);
+        System.out.println("ciao from branch");
     }
 }
